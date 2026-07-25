90e96dc6c2ab7a84fa98db4ee6ca7b4e4e13774f
wzy <wzhiying326@gmail.com>
2026-07-25 22:07:00 +0800

    first commit

---
+++ configuration.nix
@@ -0,0 +1,12 @@
+{ user, ... }:
+
+{
+  # Determinate already manages the Nix daemon, so nix-darwin shouldn't.
+  nix.enable = false;
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "aarch64-darwin"; # use x86_64-darwin for Intel CPU
+
+  system.primaryUser = "zhi";
+  system.stateVersion = 6;
+}
