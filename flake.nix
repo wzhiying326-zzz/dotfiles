8cd12c199ce427e44f4ced23787bced099b2539f
wzy <wzhiying326@gmail.com>
2026-07-26 08:18:42 +0800

    小修改

--- flake.nix
+++ flake.nix
@@ -24,7 +24,8 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.${user} = import ./home.nix;
+            home-manager.users.zhi = import ./home.nix;
+            home-manager.backupFileExtension = ".bak";
           }
         ];
       };
